c9c72d2ed7ef6b4493585657e896f4bbea21ec37
Guro <beshidzeguro@gmail.com>
2024-11-28 00:32:41 +0400

    Last corrections for now

--- src/Main.kt
+++ src/Main.kt
@@ -2,67 +2,65 @@ import java.lang.Double.parseDouble
 
 fun main() {
 
-    println("დაწყება")
-    print("შეიყვანეთ X ცვლადის მნიშვნელობა: ")
-    var x: String? = readLine()
-    print("შეიყვანეთ Y ცვლადის მნიშვნელობა: ")
-    var y: String? = readLine()
+    var userInput: Char = '0'
+    while (userInput != 'N') {
+        println("Start")
+        print("Enter the meaning of X: ")
+        var x: String? = readLine()
+        print("Enter the meaning of Y: ")
+        var y: String? = readLine()
 
-    var z: Double
-    var resultOfX: String = ""
-    var resultOfY: String = ""
+        // declared to show taken numbers from x and y
+        var z: Double
+        var resultOfX: String = ""
+        var resultOfY: String = ""
 
-    for(char in x!!) {                          // Checking each character of x and taking only numbers
-        if(char.isDigit()) {
-            resultOfX += char
+        // Checking each character of x and taking only numbers
+        for (char in x!!) {
+            if (char.isDigit()) {
+                resultOfX += char
+            }
+        }
+
+        // Checking each character of y and taking only numbers
+        for (char in y!!) {
+            if (char.isDigit()) {
+                resultOfY += char
+            }
+        }
+
+        // Checking if X contains numbers or not
+        if(!resultOfX.any { it.isDigit()}) {
+            resultOfX = "0"
+        }
+
+        // Checking if Y contains numbers or not
+        if(!resultOfY.any { it.isDigit()}) {
+            resultOfY = "0"
+        }
+
+        // converting taken numbers string to integer
+        val finalNumOfX = resultOfX.toInt()
+        val finalNumOfY = resultOfY.toInt()
+
+        // Checking if divisor is equal to zero and making division if it;s not zero
+        if (finalNumOfY == 0) {
+            println("You can't divide by zero.")
+        } else {
+            z = (finalNumOfX.toDouble() / finalNumOfY.toDouble())
+            println("$finalNumOfX divided by $finalNumOfY is: $z")
+        }
+
+        // Asking user to restart or terminate program
+        print("Do you want to restart the program? <Y/N>")
+
+        var userChoice = readLine()
+        if (userChoice != null && userChoice.length == 1) {
+            userInput = userChoice.single().uppercaseChar()
+            if (userInput == 'N') {
+                println("The end")
+                break
+            }
         }
     }
-    for(char in y!!) {                             // Checking each character of y and taking only numbers
-        if(char.isDigit()) {
-            resultOfY += char
-        }
-    }
-
-
-    println(resultOfX)                              // string of taken numbers from x
-    println(resultOfY)                              // string of taken numbers from y
-
-
-
-    val numOfX = resultOfX.toInt()
-    val numOfY = resultOfY.toInt()
-    z = (numOfX / numOfY).toDouble()
-    println(z)
-
-
-
-
-
-/*    var resultOfX: String? = null
-    var resultOfY: String? = null
-    var isXNumeric = true
-    var numOfX: Int?
-    try {
-         numOfX = x?.toIntOrNull()
-        println(numOfX)
-    } catch (e: NumberFormatException) {
-        isXNumeric = false
-    }
-    *//*if(isXNumeric) {
-        resultOfX = x?.filter { it.isDigit() }
-    }
-
-    var isYNumeric = true
-    try {
-        val numOfY = y?.toIntOrNull()
-    } catch (e: NumberFormatException) {
-        isYNumeric = false
-    }
-    if(isYNumeric) {
-        resultOfY = y?.filter { it.isDigit() }
-    }*/
-
-
-
-
 }
